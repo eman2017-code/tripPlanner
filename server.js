982f164e3a8cf823e83e6d461144b5727f5880a6
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-10-25 11:27:48 -0500

    added db folder and file

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+	console.log('Server listening on port ' + PORT);
+});
