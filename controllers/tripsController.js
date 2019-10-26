940b98ce30b457e1de2d861438281f9892ac3303
Griffin Delgado <griffinelijahd@gmail.com>
2019-10-26 17:15:37 -0500

    edit group page rendering

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -4,9 +4,16 @@ const Trip = require('../models/trip.js')
 const User = require('../models/user.js');
 const bcrypt = require('bcryptjs');
 
+//create group route
 router.get('/createGroup', (req, res) => {
 	res.render('trips/createGroup.ejs')
 })
+
+//edit group route
+router.get('/editGroup', (req, res) => {
+	res.render('trips/editGroup.ejs')
+})
+
 
 
 module.exports = router;
