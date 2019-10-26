107c2a5eae980c98b3a0e779967d972d666defcc
Griffin Delgado <griffinelijahd@gmail.com>
2019-10-26 15:37:32 -0500

    added trip controller and create group page

---
+++ controllers/tripsController.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Trip = require('../models/trip.js')
+const User = require('../models/user.js');
+const bcrypt = require('bcryptjs');
+
+
+
+
+module.exports = router;
