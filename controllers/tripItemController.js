468e0fe7d879c4bf8ce2377518746751ed432c46
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-10-29 17:29:10 -0500

    fixed delete route; now renders to homePage

--- controllers/tripItemController.js
+++ controllers/tripItemController.js
@@ -2,20 +2,46 @@ const express = require('express');
 const router = express.Router();
 const Trip = require('../models/trip.js');
 const User = require('../models/user.js');
+const TripItem = require('../models/tripItem.js');
 const bcrypt = require('bcryptjs');
 
 
+// post route
+router.post('/', (req, res) => {
+	console.log(req.body, ' this should be the contents of the body');
 
-// post route (adding items) 
-router.post('/trip/createTripHomePage/:id', (req, res) => {
-	TripItem.create(req.body, (err, createdTripItem) => {
-		if(err) {
-			res.send(err);
-		} else {
-			console.log(createdTripItem);
-		}
-	})
+    TripItem.create(req.body, (err, createdTripItem) => {
+      if (err) {
+        res.send(err)
+      } else {
+        console.log(createdTripItem);
+        // I'm getting /fruits from the url in the index route
+        // res.redirect('/fruits');
+      }
+    })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
