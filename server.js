ad61833635cccc0150e18e2b6e72b89d4fa7e090
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-10-31 12:16:28 -0500

    updated PORT for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 // dependencies
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const session = require('express-session');
